2634f9abbae39fd621ec2327625b45844a395d82
Mark Albright <kalani729@gmail.com>
2017-07-17 11:41:21 -0600

    Added new and edit buttons for portfolio items

---
+++ app/views/pages/find_max.rb
@@ -0,0 +1,15 @@
+
+require 'rspec'
+
+
+def new_max array_of_elements
+array_of_elements.sort = [2, 4, 10, 3, 1]
+
+end
+
+describe 'New Max method' do
+  it 'returns the max value from an array efficiently' do
+    test_array = [2, 4, 10, 3, 1]
+    expect(new_max(test_array)).to eq(10)
+  end
+end
