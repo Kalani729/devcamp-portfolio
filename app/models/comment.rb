d0a80b76c688d32e50d4576df23fb4475fad8e6f
Mark Albright <kalani729@gmail.com>
2017-08-07 13:40:41 -0600

    Created a comment broadcas, configured the wbsocket route, and updated the comment model with a callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :blog
 
   validates :content, presence: true, length: { minumum: 5, maximum: 1000 }
+
+  adter_create_commit { CommentBroadcastJob.perform_later(self) }
 end
