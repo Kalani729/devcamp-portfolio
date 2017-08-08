249b3a5946e92409c918485a3fdf61a1e2d8fbe8
Mark Albright <kalani729@gmail.com>
2017-08-08 12:07:30 -0600

    Built out topic views

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,9 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :topic_id
+
+  belongs_to :topic
 
   has_many :comments, dependent: :destroy
 
@@ -14,7 +16,7 @@ class Blog < ApplicationRecord
   def self.featured_blogs
     limit(2)
   end
-  
+
   def self.recent
     order("created_at DESC")
   end
